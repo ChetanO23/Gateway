a8f54dff9ccb5ee3d44f2e5b209a6366e0c52db8
ChetanO23 <hichetanmore@gmail.com>
2021-02-10 16:29:04 +0530

    CRUD Operations Assignment 1 Done

---
+++ CRUD Operations Assignments/Assignment_1_DB_First/Assignment_1_DB_First/Controllers/AddressesViewController.cs
@@ -0,0 +1,41 @@
+﻿using Assignment_1_DB_First.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment_1_DB_First.Controllers
+{
+    public class AddressesViewController : Controller
+    {
+        // GET: AddressesView
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult viewaddresses()
+        {
+            ViewBag.Message = "User's Addresses Details";
+            CRUD_OP_Training_SessionEntities db = new CRUD_OP_Training_SessionEntities();
+            List<Address> addresslist = db.Addresses.ToList();
+            AddressesViewModel avm = new AddressesViewModel();
+            List<AddressesViewModel> avmlist = addresslist.Select(x => new AddressesViewModel
+
+            {
+                ID = x.ID,
+                USER_NAME = x.User.USER_NAME,
+                USER_ID=x.USER_ID,
+                ADDRESS_TYPE=x.ADDRESS_TYPE,
+                ADDRESS_VALUE=x.ADDRESS_VALUE
+            }
+
+
+            ).ToList();
+
+
+            return View(avmlist);
+        }
+    }
+}
---
+++ CRUD Operations Assignments/Assignment_1_DB_First/Assignment_1_DB_First/Models/AddressesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment_1_DB_First.Models
+{
+    public class AddressesViewModel
+    {
+        public int ID { get; set; }
+        public int USER_ID { get; set; }
+        public string ADDRESS_TYPE { get; set; }
+        public string ADDRESS_VALUE { get; set; }
+        public string USER_NAME { get; set; }
+
+
+    }
+}
