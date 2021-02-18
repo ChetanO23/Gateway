21590d308e387b5e188372d05e28bc75f9d7c90c
ChetanO23 <hichetanmore@gmail.com>
2021-02-18 19:07:23 +0530

    Crud Operations Training Assignment 3 Done

---
+++ CRUD Operations Assignments/Assignment_3_Code_First/Code_First_Approach/Code_First_Demo.Model/KeyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Code_First_Demo.Model
+{
+    internal class KeyAttribute : Attribute
+    {
+    }
+}
---
+++ CRUD Operations Assignments/Assignment_3_Code_First/Code_First_Approach/Code_First_Demo.Repository/CDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Code_First_Demo.Model;
+
+namespace Code_First_Demo.Repository
+{
+    public class CDBContext : DbContext
+    {
+        public CDBContext() : base("StringDBContext")
+        {
+
+        }
+        public DbSet<MyFirstTable> MyFirstTable { get; set; }
+    }
+}
