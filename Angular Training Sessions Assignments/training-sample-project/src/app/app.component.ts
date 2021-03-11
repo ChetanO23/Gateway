6df3bcd7e0d19dccc08e05fb1e59b1c9a3039843
ChetanO23 <hichetanmore@gmail.com>
2021-03-11 08:58:55 +0530

    View Child

--- Angular Training Sessions Assignments/training-sample-project/src/app/app.component.ts
+++ Angular Training Sessions Assignments/training-sample-project/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { ChildComponent } from './child/child.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild(ChildComponent) child: ChildComponent;
+  ngAfterViewInit() {
+    console.log(this.child.whoAmI()); // message print output
+    return this.child.whoAmI();
+  }
   title = 'training-sample-project';
 }
