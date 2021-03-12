8194676c1e64786868a902e129bb5726f6434f21
ChetanO23 <hichetanmore@gmail.com>
2021-03-12 08:39:53 +0530

    routing

--- Angular Training Sessions Assignments/training-sample-project/src/app/app.module.ts
+++ Angular Training Sessions Assignments/training-sample-project/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingcomponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+//import { DepartmentComponent } from './department/department.component';
+//import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+   // DepartmentComponent,
+    //EmployeeComponent
+    routingcomponent,
+   PageNotFoundComponent,
+   HomeComponent 
   ],
   imports: [
     BrowserModule,
