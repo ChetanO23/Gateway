a24e6bb889d018057495c8d766fc2ade9dfcf07a
ChetanO23 <60738483+ChetanO23@users.noreply.github.com>
2021-03-30 19:05:02 +0530

    Added

---
+++ .Net Core Training Assignments/.Net Core Final Assignment/DotNetCore_HRM/HRM.BAL/Interfaces/IEmployeeManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM.BAL.Interfaces
+{
+   public  interface IEmployeeManager
+    {
+    }
+}
---
+++ .Net Core Training Assignments/.Net Core Final Assignment/DotNetCore_HRM/HRM.DAL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM.DAL.Interfaces
+{
+   public interface IDepartmentRepository
+    {
+        IEnumerable 
+    }
+}
